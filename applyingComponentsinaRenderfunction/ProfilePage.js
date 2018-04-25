66ddc13c4e6e3c88bb3d72b511d2839f191e2d6a
JOHNFLEURIMOND <johnnyxxboston@gmail.com>
2018-04-25 18:49:07 -0400

    React Practice

---
+++ applyingComponentsinaRenderfunction/ProfilePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+class ProfilePage extends React.Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <h1>All About Me!</h1>
+        <p>I like movies and blah blah blah blah blah</p>
+        <img src="https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-monkeyselfie.jpg" />
+      </div>
+    );
+  }
+}
